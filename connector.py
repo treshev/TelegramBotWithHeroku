b8697f71274e6e0354688ad17b5041f15b04a367
treshchev <a.treshev@gmail.com>
2018-07-26 11:43:06 +0200

    init commit for heroku

---
+++ connector.py
@@ -0,0 +1,111 @@
+import datetime
+import json
+from collections import defaultdict, OrderedDict
+
+import redis
+
+START, ADD_ADDRESS, IS_PHOTO_NEEDED, ADD_PHOTO, IS_LOCATION_NEEDED, ADD_LOCATION, END = range(7)
+USER_STATE = defaultdict(lambda: START)
+USER_DATA = {}
+
+
+class Connector:
+    def get_user_data(self, chat_id):
+        raise NotImplementedError
+
+    def update_user_data(self, chat_id, key, value):
+        raise NotImplementedError
+
+    def commit_user_data(self, chat_id):
+        raise NotImplementedError
+
+    def delete_user_data(self, chat_id):
+        raise NotImplementedError
+
+    def get_state(self, chat_id):
+        raise NotImplementedError
+
+    def update_state(self, chat_id, state):
+        raise NotImplementedError
+
+
+class RedisConnector(Connector):
+    redis_connection = None
+
+    def get_redis_connection(self):
+        if self.redis_connection is None:
+            self.redis_connection = redis.StrictRedis(host='localhost', port=6379, db=0)
+        return self.redis_connection
+
+    def get_user_data(self, chat_id):
+        r = self.get_redis_connection()
+        user_binary_data = r.get(chat_id)
+        if user_binary_data:
+            user_data = json.loads(user_binary_data)
+            ordered_list = OrderedDict(sorted(user_data.items(), reverse=True))
+            return ordered_list
+        else:
+            return None
+
+    def update_user_data(self, chat_id, key, value):
+        r = self.get_redis_connection()
+        user = r.get(chat_id)
+        if not user:
+            user_data = {"current": {key: value}}
+        else:
+            user_data = json.loads(user)
+            if isinstance(user_data['current'], str):
+                user_data["current"] = {key: value}
+            else:
+                user_data["current"][key] = value
+        print(user_data)
+        r.set(chat_id, json.dumps(user_data))
+
+    def commit_user_data(self, chat_id):
+        r = self.get_redis_connection()
+        user = r.get(chat_id)
+        if user:
+            user_data = json.loads(user)
+            user_data[str(datetime.datetime.now())] = user_data.get('current', None)
+            user_data['current'] = ''
+            r.set(chat_id, json.dumps(user_data))
+
+    def delete_user_data(self, chat_id):
+        r = self.get_redis_connection()
+        r.delete(chat_id)
+
+    def get_state(self, chat_id):
+        r = self.get_redis_connection()
+        user_state_data = r.get("USER_STATE_{}".format(chat_id))
+        user_data = int(user_state_data) if user_state_data else None
+        return user_data
+
+    def update_state(self, chat_id, state):
+        r = self.get_redis_connection()
+        r.set("USER_STATE_{}".format(chat_id), state)
+
+
+class InMemoryConnector(Connector):
+
+    def get_user_data(self, chat_id):
+        return USER_DATA.get(chat_id.chat.id, None)
+
+    def update_user_data(self, chat_id, key, value):
+        if chat_id not in USER_DATA:
+            USER_DATA[chat_id] = {"current": {key: value}}
+        else:
+            USER_DATA[chat_id]["current"][key] = value
+
+    def commit_user_data(self, chat_id):
+        if chat_id in USER_DATA:
+            USER_DATA[chat_id][str(datetime.datetime.now())] = USER_DATA[chat_id].get('current', None)
+            USER_DATA[chat_id]['current'] = {}
+
+    def delete_user_data(self, chat_id):
+        USER_DATA.pop(chat_id, "Empty")
+
+    def get_state(self, chat_id):
+        return USER_STATE[chat_id]
+
+    def update_state(self, chat_id, state):
+        USER_STATE[chat_id] = state
---
+++ telegram_bot.py
@@ -0,0 +1,196 @@
+import logging
+import os
+
+import connector
+import telebot
+from telebot import types
+
+from flask import Flask, request
+
+
+START, ADD_ADDRESS, IS_PHOTO_NEEDED, ADD_PHOTO, IS_LOCATION_NEEDED, ADD_LOCATION, END = range(7)
+
+token = '616278926:AAHKgVpqO8vo1kSPyTmFE76X7h3AIE6meII'
+bot = telebot.TeleBot(token)
+
+IS_REDIS = True
+connection = connector.RedisConnector()
+
+
+def create_keyboard():
+    keyboard = types.InlineKeyboardMarkup(row_width=3)
+    buttons = [types.InlineKeyboardButton(text="add", callback_data="add_action"),
+               types.InlineKeyboardButton(text="list", callback_data="list_action"),
+               types.InlineKeyboardButton(text="reset", callback_data="reset_action")]
+    keyboard.add(*buttons)
+    return keyboard
+
+
+def create_yes_no_keyboard():
+    keyboard = types.InlineKeyboardMarkup(row_width=3)
+    buttons = [types.InlineKeyboardButton(text="Yes", callback_data="yes"),
+               types.InlineKeyboardButton(text="No", callback_data="no")]
+    keyboard.add(*buttons)
+    return keyboard
+
+
+@bot.message_handler(commands=['start'])
+def handle_start_message(message):
+    keyboard = create_keyboard()
+    bot.send_message(chat_id=message.chat.id, text="Hello my friend. Please choose what you wonna do",
+                     reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda
+        callback_query: callback_query.data == 'add_action' or callback_query.data == 'list_action' or callback_query.data == 'reset_action')
+def handle_initial_commands(callback_query):
+    if callback_query.data == 'add_action':
+        handle_add_command(callback_query.message)
+    elif callback_query.data == 'list_action':
+        handle_list_command(callback_query.message)
+    elif callback_query.data == 'reset_action':
+        handle_reset_command(callback_query.message)
+
+
+@bot.message_handler(commands=['add'])
+def handle_add_command(message):
+    text = message.text.split(" ")
+    if "add" in message.text and len(text) > 1:
+        keyboard = create_yes_no_keyboard()
+        connection.update_user_data(message.chat.id, "address", message.text[5:])
+        connection.update_state(message.chat.id, IS_PHOTO_NEEDED)
+        bot.send_message(chat_id=message.chat.id, text="Хотите добавить фото?",
+                         reply_markup=keyboard)
+    else:
+        connection.update_state(message.chat.id, ADD_ADDRESS)
+        bot.send_message(chat_id=message.chat.id, text="Введите адрес")
+
+
+@bot.callback_query_handler(func=lambda message: connection.get_state(message.message.chat.id) == IS_PHOTO_NEEDED)
+def callback_handler(callback_query):
+    data = callback_query.data
+    message = callback_query.message
+    if data == 'yes':
+        connection.update_state(message.chat.id, ADD_PHOTO)
+        bot.send_message(message.chat.id, text='Пожалуйста загрузите фото')
+    else:
+        keyboard = create_yes_no_keyboard()
+        connection.update_state(message.chat.id, IS_LOCATION_NEEDED)
+        bot.send_message(chat_id=message.chat.id, text="Хотите добавить локейшен?",
+                         reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda message: connection.get_state(message.message.chat.id) == IS_LOCATION_NEEDED)
+def callback_handler(callback_query):
+    data = callback_query.data
+    message = callback_query.message
+    if data == 'yes':
+        connection.update_state(message.chat.id, ADD_LOCATION)
+        bot.send_message(message.chat.id, text='Пожалуйста добавьте локацию')
+    else:
+        connection.update_state(message.chat.id, END)
+        connection.commit_user_data(message.chat.id)
+        bot.send_message(message.chat.id, text='Адресс был успешно добавлен')
+
+
+@bot.message_handler(func=lambda message: connection.get_state(message.chat.id) == ADD_ADDRESS)
+def handle_address_message(message):
+    if message.text and "list" not in message.text and "reset" not in message.text:
+        connection.update_user_data(message.chat.id, "address", message.text)
+        keyboard = create_yes_no_keyboard()
+        connection.update_state(message.chat.id, IS_PHOTO_NEEDED)
+        bot.send_message(chat_id=message.chat.id, text="Хотите добавить фото?",
+                         reply_markup=keyboard)
+    else:
+        connection.update_state(message.chat.id, START)
+
+
+@bot.message_handler(content_types=['photo'], func=lambda message: connection.get_state(message.chat.id) == ADD_PHOTO)
+def handle_photo(message):
+    file_id = message.photo[-1].file_id
+    connection.update_user_data(message.chat.id, "file_id", file_id)
+
+    keyboard = create_yes_no_keyboard()
+    connection.update_state(message.chat.id, IS_LOCATION_NEEDED)
+    bot.send_message(chat_id=message.chat.id, text="Хотите добавить локейшен?",
+                     reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=['location'],
+                     func=lambda message: connection.get_state(message.chat.id) != ADD_LOCATION)
+def handle_photo(message):
+    bot.send_message(message.chat.id, text='Объект был успешно добавлен')
+
+
+@bot.message_handler(content_types=['location'],
+                     func=lambda message: connection.get_state(message.chat.id) == ADD_LOCATION)
+def handle_photo(message):
+    location = message.location
+    connection.update_user_data(message.chat.id, "location", [location.latitude, location.longitude])
+    connection.commit_user_data(message.chat.id)
+    bot.send_message(message.chat.id, text='Объект был успешно добавлен')
+
+
+@bot.message_handler(commands=['list'])
+def handle_list_command(message):
+    places_list = connection.get_user_data(message.chat.id)
+    if places_list:
+        i = 1
+        for place in places_list:
+            if 'current' not in place:
+                item = places_list[place]
+                title_text = "<b>{}</b>. {}".format(i, item["address"])
+                bot.send_message(chat_id=message.chat.id, text=title_text, parse_mode="HTML")
+
+                if "file_id" in item.keys():
+                    bot.send_photo(chat_id=message.chat.id, photo=item["file_id"])
+
+                if "location" in item.keys():
+                    location = item["location"]
+                    bot.send_location(message.chat.id, location[0], location[1])
+                if i < 10:
+                    i += 1
+                else:
+                    return
+
+    else:
+        bot.send_message(chat_id=message.chat.id,
+                         text="У вас нет добавленных объектов")
+
+
+@bot.message_handler(commands=['reset'])
+def handle_reset_command(message):
+    connection.delete_user_data(message.chat.id)
+    bot.send_message(chat_id=message.chat.id, text="Заметки были успешно удалены")
+
+
+if __name__ == '__main__':
+    if "HEROKU" in list(os.environ.keys()):
+        logger = telebot.logger
+        telebot.logger.setLevel(logging.INFO)
+
+        server = Flask(__name__)
+
+
+        @server.route("/bot", methods=['POST'])
+        def getMessage():
+            bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+            return "!", 200
+
+
+        @server.route("/")
+        def webhook():
+            bot.remove_webhook()
+            bot.set_webhook(
+                url="https://alekssmrbot.herokuapp.com/bot")  # этот url нужно заменить на url вашего Хероку приложения
+            return "?", 200
+
+
+        server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+
+        bot.polling()
+    else:
+        # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+        # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+        bot.remove_webhook()
+        bot.polling(none_stop=True)
